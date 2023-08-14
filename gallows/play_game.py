c91bdc4d06b41790090564af5774c23ae87e1276
Alisa2Chev <ilya.baykov.1980@mail.ru>
2023-08-15 00:25:35 +0300

    Made the launch of all the necessary functions through the initialization of the class

--- gallows/play_game.py
+++ gallows/play_game.py
@@ -4,38 +4,31 @@ from fiile_preparation import FileEditor
 
 
 class MainGame(FileEditor):
-    LIFE_POINT = 7
 
-    @classmethod
-    def get_random_word(cls):
-        random_word = cls.read_file("Words_raw")
+    def __init__(self, life_point=7, word=""):
+        self.life_point = life_point
+        self.word = word if word else self.get_random_word()
+        self.letter_list = [((5 * "\u2014") + "\t") for i in range(len(self.word))]
+        print(self.letter_list)
+        self.start_game()
+
+    def get_random_word(self):
+        random_word = self.read_file("Words_raw")
         return choice(random_word)
 
-    @staticmethod
-    def get_length(_word: str):
-        letter_list = [len(_word) * ((5 * "\u2014") + "\t")]
-        print(*letter_list)
-        return letter_list
+    def substitution_letters(self, _letter) -> None:
+        for char in range(len(self.word)):
+            if self.word[char] == _letter:
+                self.letter_list[char] = _letter
 
-    @staticmethod
-    def substitution_letters(_word, _letter, letter_list):
-        for char in range(len(_word)):
-            if _word[char] == _letter:
-                letter_list[char] = _letter
-        return letter_list
-
-    @staticmethod
-    def start_game(_word, _letter_list, life_point=LIFE_POINT):
-        while life_point > 0:
+    def start_game(self):
+        while self.life_point > 0 or "\u2014" not in self.letter_list:
             letter = input("Введите букву ")
-            if letter in _word:
-                print("ДА!!!")
+            if letter in self.word:
+                self.substitution_letters(letter)
+                print(self.letter_list)
             else:
-                life_point -= 1
+                self.life_point -= 1
 
 
 game = MainGame()
-word = game.get_random_word()
-print(word)
-
-game.start_game(word, game.get_length(word))
