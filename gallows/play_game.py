bfa2e4c66e3aa5e15e42020bcb9a944fa7121cce
Alisa2Chev <ilya.baykov.1980@mail.ru>
2023-08-15 13:26:26 +0300

    Added a method that draws the game process

--- gallows/play_game.py
+++ gallows/play_game.py
@@ -6,6 +6,7 @@ from fiile_preparation import FileEditor
 class MainGame(FileEditor):
 
     def __init__(self):
+        self.story_letters = []
         self.__parameters = self.__get_parameters()
         self.__word = self.__parameters[0]
         self.__life_point = self.__parameters[1]
@@ -33,17 +34,42 @@ class MainGame(FileEditor):
     def __start_game(self):
         while self.__life_point > 0:
             letter = input("Введите букву ")
+            if letter not in self.story_letters:
+                self.story_letters.append(letter)
+            else:
+                print("Вы уже вводили эту букву...")
+
             if letter in self.__word:
-                self.substitution_letters(letter)
+                self.__substitution_letters(letter)
                 print(*self.__letter_list)
                 if '—————\t' not in self.__letter_list:
                     self.__loose = True
                     break
             else:
                 self.__life_point -= 1
+                self.draw()
+
         self.__total()
 
-    def substitution_letters(self, _letter) -> None:
+    def draw(self):
+        print(f"    [------------------")
+        print(f"    [                {'|' if self.__life_point < 7 else ''} ")
+        print(f"    [                {'|' if self.__life_point < 7 else ''} ")
+        print(f"    [              {' ___' if self.__life_point < 6 else ''}")
+        print("    [              " + ('/   \\' if self.__life_point < 6 else " "))
+        print("    [              " + ('\\   /' if self.__life_point < 6 else " "))
+        print(f"    [              {' ___' if self.__life_point < 6 else ''}")
+        print(f"    [              {'/' if self.__life_point < 4 else ''}  {'|' if self.__life_point < 5 else ''} " + (
+            "\\" if self.__life_point < 3 else ""))
+        print(f"    [             {'/' if self.__life_point < 4 else ''}   {'|' if self.__life_point < 5 else ''}  " + (
+            "\\" if self.__life_point < 3 else ""))
+        print(f"    [                {'|' if self.__life_point < 5 else ''} ")
+        print(f"    [                {'|' if self.__life_point < 5 else ''} ")
+        print(f"    [               {'/' if self.__life_point < 2 else ''}" + (" \\" if self.__life_point < 1 else ""))
+        print(f"    [              {'/' if self.__life_point < 2 else ''}" + ("   \\" if self.__life_point < 1 else ""))
+        print(f"____[_______")
+
+    def __substitution_letters(self, _letter) -> None:
         for char in range(len(self.__word)):
             if self.__word[char] == _letter:
                 self.__letter_list[char] = _letter
