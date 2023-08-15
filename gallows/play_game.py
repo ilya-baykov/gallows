6c21b60be9ba1b74f8db27dbe08923ebe0326ab7
Alisa2Chev <ilya.baykov.1980@mail.ru>
2023-08-15 14:24:55 +0300

    version 0.95

--- gallows/play_game.py
+++ gallows/play_game.py
@@ -16,15 +16,10 @@ class MainGame(FileEditor):
         self.__start_game()
 
     def __get_parameters(self) -> list:
-        if "n" in input("Вы хотите сыграть с комьютером ? ( Y / N )\t\t"):
-            _word = input("Загадайте слово второму игроку:\t ")
-        else:
-            _word = self.__get_random_word()
+        _word = input("Введите слово для другого игрока или нажмите enter, чтобы сыграть с компьютером\t")
+        _word = _word if isinstance(_word, str) and _word.strip() else self.__get_random_word()
         _life_pointer = input("Введите количество жизней: ( по умолчанию - 7 )\t")
-        if _life_pointer.isdigit():
-            _life_pointer = int(_life_pointer)
-        else:
-            _life_pointer = 7
+        _life_pointer = int(_life_pointer) if _life_pointer.isdigit() and 8 > int(_life_pointer) > 1 else 7
         return [_word, _life_pointer]
 
     def __get_random_word(self):
@@ -36,17 +31,19 @@ class MainGame(FileEditor):
             letter = input("Введите букву ")
             if letter not in self.story_letters:
                 self.story_letters.append(letter)
+                print(f"Список введенных букв: {self.story_letters}")
             else:
                 print("Вы уже вводили эту букву...")
 
             if letter in self.__word:
                 self.__substitution_letters(letter)
-                print(*self.__letter_list)
+                print(*self.__letter_list, end="\n\n")
                 if '—————\t' not in self.__letter_list:
                     self.__loose = True
                     break
             else:
                 self.__life_point -= 1
+                print(*self.__letter_list, end="\n\n")
                 self.draw()
 
         self.__total()
@@ -59,20 +56,24 @@ class MainGame(FileEditor):
         print("    [              " + ('/   \\' if self.__life_point < 6 else " "))
         print("    [              " + ('\\   /' if self.__life_point < 6 else " "))
         print(f"    [              {' ___' if self.__life_point < 6 else ''}")
-        print(f"    [              {'/' if self.__life_point < 4 else ''}  {'|' if self.__life_point < 5 else ''} " + (
+        print(f"    [                {'|' if (self.__life_point == 4) else ''}")
+        print(f"    [              {'/' if self.__life_point < 4 else ''}  {'|' if self.__life_point == 4 else ''} " + (
             "\\" if self.__life_point < 3 else ""))
-        print(f"    [             {'/' if self.__life_point < 4 else ''}   {'|' if self.__life_point < 5 else ''}  " + (
-            "\\" if self.__life_point < 3 else ""))
+        print(
+            f"    [             {'/' if self.__life_point < 4 else ''}   {'|' if self.__life_point == 4 else ''}  " + (
+                "\\" if self.__life_point < 3 else ""))
         print(f"    [                {'|' if self.__life_point < 5 else ''} ")
         print(f"    [                {'|' if self.__life_point < 5 else ''} ")
         print(f"    [               {'/' if self.__life_point < 2 else ''}" + (" \\" if self.__life_point < 1 else ""))
         print(f"    [              {'/' if self.__life_point < 2 else ''}" + ("   \\" if self.__life_point < 1 else ""))
         print(f"____[_______")
+        print(f"У вас осталось {self.__life_point} попыток")
+        print()
 
     def __substitution_letters(self, _letter) -> None:
         for char in range(len(self.__word)):
             if self.__word[char] == _letter:
-                self.__letter_list[char] = _letter
+                self.__letter_list[char] = _letter + "\t"
 
     def __total(self):
         print("Ты выиграл!!!" if self.__loose else f"Ты проиграл ;(\nЗагаданное слово - {self.__word}")
